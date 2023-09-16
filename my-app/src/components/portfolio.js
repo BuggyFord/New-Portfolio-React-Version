3728a0bfbc0ad15db22ef22b8af17fe8eadaacc5
BuggyFord <128647975+BuggyFord@users.noreply.github.com>
2023-09-16 12:43:05 -0500

    added import to get pictures of apps and set correct pic to app

--- my-app/src/components/portfolio.js
+++ my-app/src/components/portfolio.js
@@ -1,52 +1,57 @@
 import React, {useState} from "react";
 import projectPic from '../images/Screenshot 2023-09-12 at 6.14.16 PM.png';
-//import projectPic2 from '../images/Screenshot 2023-09-12 at 6.15.03 PM.png'
+import projectPic2 from '../images/Screenshot 2023-09-12 at 6.15.03 PM.png';
+import projectPic3 from '../images/Screenshot 2023-09-12 at 6.15.46 PM.png';
+import projectPic4 from '../images/Screenshot 2023-09-12 at 6.16.17 PM.png';
+import projectPic5 from '../images/Screenshot 2023-09-12 at 6.19.02 PM.png';
+import projectPic6 from '../images/Screenshot 2023-09-12 at 6.20.33 PM.png';
 import { Link } from 'react-router-dom';
 
 function Portfolio() {
     let projectsArr = [
 {
     title:'PWA',
-    image: '/Users/tylerjohnson/Desktop/bootcamp2/challenges/Challenge-FinalHW/New-Portfolio-React-Version/my-app/public/images/Screenshot 2023-09-12 at 6.16.17 PM.png',
+    image: projectPic4,
     link: 'https://shielded-crag-21090-08b3f3cf5127.herokuapp.com/ ',
     github_link: 'https://github.com/BuggyFord/Num-19'
 
 },
 {
     title:'Borderline Incredible Weather App',
-    image: '/Users/tylerjohnson/Desktop/bootcamp2/challenges/Challenge-FinalHW/New-Portfolio-React-Version/my-app/public/images/Screenshot 2023-09-12 at 6.19.02 PM.png',
+    image: projectPic5,
     link: 'https://buggyford.github.io/Bordeline-Incredible-WeatherAPP/',
     github_link: 'https://github.com/BuggyFord/Bordeline-Incredible-WeatherAPP'
 
 },
 {
     title:'BA Travel',
-    image: '/Users/tylerjohnson/Desktop/bootcamp2/challenges/Challenge-FinalHW/New-Portfolio-React-Version/my-app/public/images/Screenshot 2023-09-12 at 6.15.46 PM.png',
-    link: ' https://pzhong1.github.io/BA-travel-/Links to an external site.',
-    github_link: 'https://github.com/pzhong1/BA-travel-Links to an external site.'
+    image: projectPic3,
+    link: ' https://pzhong1.github.io/BA-travel-',
+    github_link: 'https://github.com/pzhong1/BA-travel-'
 
 },
 {
     title:'Work Day Scheduler',
-    image: '/Users/tylerjohnson/Desktop/bootcamp2/challenges/Challenge-FinalHW/New-Portfolio-React-Version/my-app/public/images/Screenshot 2023-09-12 at 6.15.03 PM.png',
-    link: 'https://buggyford.github.io/Work-Day-9-5-power-hours/Links to an external site.',
-    github_link: 'https://github.com/BuggyFord/Work-Day-9-5-power-hours.gitLinks to an external site.'
+    image: projectPic2,
+    link: 'https://buggyford.github.io/Work-Day-9-5-power-hours/',
+    github_link: 'https://github.com/BuggyFord/Work-Day-9-5-power-hours.git'
 
 },
 {
     title:'Code Quiz',
-    image: 'my-app/public/images/Screenshot 2023-09-12 at 6.20.33 PM.png',
-    link: 'https://buggyford.github.io/WIcked-Cool-Coding-Quiz/Links to an external site.',
-    github_link: 'https://github.com/BuggyFord/WIcked-Cool-Coding-Quiz.gitLinks to an external site.'
+    image: projectPic6,
+    link: 'https://buggyford.github.io/WIcked-Cool-Coding-Quiz/',
+    github_link: 'https://github.com/BuggyFord/WIcked-Cool-Coding-Quiz.git'
 
 },
 {
     title:'Rocking Password Generator',
-    image: 'images/Screenshot 2023-09-12 at 6.14.16 PM.png',
-    link: ' https://buggyford.github.io/Tyler-Rockin-Password-Generator/Links to an external site.',
-    github_link: 'https://github.com/BuggyFord/Tyler-Rockin-Password-GeneratorLinks to an external site.'
+    image: projectPic,
+    link: ' https://buggyford.github.io/Tyler-Rockin-Password-Generator/',
+    github_link: 'https://github.com/BuggyFord/Tyler-Rockin-Password-Generator'
 
-}
+}, 
+
     ]
 
 
@@ -63,10 +68,10 @@ function Portfolio() {
                 {projects.map(item => (
                     <div>
                         <p>{item.title}</p>
-                        <img src={projectPic}></img>
+                        <img src={item.image}></img>
                         <a href={projects.link} target="_blank"/>
                         <a href={item.github_link} target="_blank"/>
-                        <Link to={item.github_link} target="_blank" rel="noreferrer"/>
+                        <Link to={item.link} target="_blank" rel="noreferrer"/>GitHub<Link/>
 
                         <p>{item.link}</p>
                         <p>{item.github_link}</p>
