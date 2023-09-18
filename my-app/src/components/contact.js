b64b746d8dd4ff8ec247b56205265b1c7fcdf8e5
BuggyFord <128647975+BuggyFord@users.noreply.github.com>
2023-09-17 20:30:31 -0500

    added to helper function to capitalize first letter, added link to resume and pdf in for resume, reworked logic for contact to have email validation and required fields with name and a message, added pdf react

--- my-app/src/components/contact.js
+++ my-app/src/components/contact.js
@@ -1,24 +1,27 @@
 import { useState } from "react";
+import { validateEmail } from "../utils/helper";
 
 /*import React from "react";
 import { checkPassword } from '../utils/helper';
 */
 function Contact() {
-    const [ currentForm, setCurrentForm] = useState("Send")
-    const onSubmit = (e) => {
-        e.preventDefault()
-        setCurrentForm('Submitting...')
-        const { name, email, message } = e.target.elements
-        let conFom = {
-            name: name.value,
-            email:email.value,
-            message:message.value,
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+
+    const onSubmit = (e) =>{
+        e.preventDefault();
+        if(!message){
+            alert("Must input something in field");
+            return;
         }
-        console.log(conFom)
-    }
-
-    
-
+        if(validateEmail(email)) {
+            alert("submitted");
+        }
+        else {
+            alert("Invalid Email");
+        }
+    };
 
     return(
         <div>
@@ -28,22 +31,39 @@ function Contact() {
                     <label>
                         Name
                     </label>
-                    <input type="text" placeholder="Name"required/>
+                    <input 
+                    type="text"
+                    id="name"
+                    placeholder="Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    />
                 </div>
                 <div>
                     <label htmlFor="email">
                         Email
                     </label>
-                    <input type="email" placeholder="Email"required/>
+                    <input
+                    type="email" 
+                    id="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    />
                 </div>
                 <div>
                     <label htmlFor="message">
                         Message
                     </label>
-                    <textarea required/>
+                    <textarea
+                    id="message"
+                    placeholder="Please enter a message"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    />
                 </div>
                 <button type="submit">
-                    {currentForm}
+                    
                 </button>
             </form>
         </div>
--- my-app/src/components/resume.js
+++ my-app/src/components/resume.js
@@ -3,7 +3,25 @@ import React from "react";
 function Resume() {
     return(
         <div>
-            <h1>Resume Component</h1>
+            <h1>Download My Resume</h1>
+            <div>
+            <a
+            href={process.env.PUBLIC_URL + "/images/updatedResume.pdf"}
+          download="updatedResume.pdf">
+            Download My Resume{" "}
+          </a>
+            </div>
+
+        <div>
+            <h2>Skills</h2>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Javascript</li>
+            <li>Sql</li>
+            <li>MongoDb</li>
+            <li>ReactJs</li>
+            <li>NodeJs</li>
+        </div>
         </div>
     )
  }
